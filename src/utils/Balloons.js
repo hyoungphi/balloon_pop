0d39d00ca4ce47f08ca4a62535bfd7a8212414b4
hyoungphi <hyoungphi@gmail.com>
2024-02-11 15:12:43 +0900

    change tab size

--- src/utils/Balloons.js
+++ src/utils/Balloons.js
@@ -1,109 +1,109 @@
 class Baloons {
-    /**
-     * @param {Map<int, Map<int, int>>} locations
-     * @param {Array<int>} dimensions
-     */
-    constructor(locations, dimensions) {
-        // TODO: Check that the Map is of the correct type
-        console.assert(locations instanceof Map, 'Locations must be a Map');
-        console.assert(dimensions.length === 2, 'Dimensions must be an array of length 2');
-        console.assert(dimensions[0] > 0, 'First dimension must be greater than 0');
-        console.assert(dimensions[1] > 0, 'Second dimension must be greater than 0');
-        this.locations = locations;
-        this.dimensions = dimensions;
-        this.maxPop = this.#calculateMaxPop();
+  /**
+   * @param {Map<int, Map<int, int>>} locations
+   * @param {Array<int>} dimensions
+   */
+  constructor(locations, dimensions) {
+    // TODO: Check that the Map is of the correct type
+    console.assert(locations instanceof Map, 'Locations must be a Map');
+    console.assert(dimensions.length === 2, 'Dimensions must be an array of length 2');
+    console.assert(dimensions[0] > 0, 'First dimension must be greater than 0');
+    console.assert(dimensions[1] > 0, 'Second dimension must be greater than 0');
+    this.locations = locations;
+    this.dimensions = dimensions;
+    this.maxPop = this.#calculateMaxPop();
+  }
+  /**
+   * @param {int} x
+   * @param {int} y
+   * @returns {boolean}
+   */
+  #checkMaxPop(x, y) {
+    let pop = this.#calculatePop(x, y);
+    if (pop < this.maxPop) {
+      return false;
     }
-    /**
-     * @param {int} x
-     * @param {int} y
-     * @returns {boolean}
-     */
-    #checkMaxPop(x, y) {
+    return true;
+  }
+
+  #calculatePop(x, y) {
+    let pop = 0;
+    if (!this.locations.get(x) || !this.locations.get(x).get(y)) {
+      return pop;
+    }
+
+    let xs = [x - 1, x, x + 1].filter((x) => x >= 0 && x < this.dimensions[0]);
+    let ys = [y - 1, y, y + 1].filter((y) => y >= 0 && y < this.dimensions[1]);
+    for (let i = 0; i < xs.length; i++) {
+      if (this.locations.get(xs[i]) && this.locations.get(xs[i]).get(y)) {
+        pop++;
+      }
+    }
+    for (let j = 0; j < ys.length; j++) {
+      if (this.locations.get(x) && this.locations.get(x).get(ys[j])) {
+        pop++;
+      }
+    }
+    if (this.locations.get(x) && this.locations.get(x).get(y)) pop--;
+
+    return pop;
+  }
+
+  #calculateMaxPop() {
+    let maxPop = 0;
+
+    for (let [x, row] of this.locations.entries()) {
+      for (let [y, _] of row.entries()) {
         let pop = this.#calculatePop(x, y);
-        if (pop < this.maxPop) {
-            return false;
-        }
-        return true;
+        maxPop = Math.max(maxPop, pop);
+        if (maxPop === 5) return maxPop;
+      }
     }
 
-    #calculatePop(x, y) {
-        let pop = 0;
-        if (!this.locations.get(x) || !this.locations.get(x).get(y)) {
-            return pop;
-        }
+    return maxPop;
+  }
 
-        let xs = [x - 1, x, x + 1].filter((x) => x >= 0 && x < this.dimensions[0]);
-        let ys = [y - 1, y, y + 1].filter((y) => y >= 0 && y < this.dimensions[1]);
-        for (let i = 0; i < xs.length; i++) {
-            if (this.locations.get(xs[i]) && this.locations.get(xs[i]).get(y)) {
-                pop++;
-            }
-        }
-        for (let j = 0; j < ys.length; j++) {
-            if (this.locations.get(x) && this.locations.get(x).get(ys[j])) {
-                pop++;
-            }
-        }
-        if (this.locations.get(x) && this.locations.get(x).get(y)) pop--;
+  /**
+   * returns new baloons after popping the baloon at the given location.
+   * If the pop is not maximum, returns null.
+   * @param {int} x
+   * @param {int} y
+   * @returns {Baloons | null}
+   */
+  doPop(x, y) {
+    console.assert(Number.isInteger(x), 'x must be an integer');
+    console.assert(Number.isInteger(y), 'y must be an integer');
+    console.assert(x >= 0, 'x must be greater than or equal to 0');
+    console.assert(x < this.dimensions[0], 'x must be less than the first dimension');
+    console.assert(y >= 0, 'y must be greater than or equal to 0');
+    console.assert(y < this.dimensions[1], 'y must be less than the second dimension');
 
-        return pop;
-    }
-
-    #calculateMaxPop() {
-        let maxPop = 0;
-
-        for (let [x, row] of this.locations.entries()) {
-            for (let [y, _] of row.entries()) {
-                let pop = this.#calculatePop(x, y);
-                maxPop = Math.max(maxPop, pop);
-                if (maxPop === 5) return maxPop;
-            }
-        }
-
-        return maxPop;
-    }
-
-    /**
-     * returns new baloons after popping the baloon at the given location.
-     * If the pop is not maximum, returns null.
-     * @param {int} x
-     * @param {int} y
-     * @returns {Baloons | null}
-     */
-    doPop(x, y) {
-        console.assert(Number.isInteger(x), 'x must be an integer');
-        console.assert(Number.isInteger(y), 'y must be an integer');
-        console.assert(x >= 0, 'x must be greater than or equal to 0');
-        console.assert(x < this.dimensions[0], 'x must be less than the first dimension');
-        console.assert(y >= 0, 'y must be greater than or equal to 0');
-        console.assert(y < this.dimensions[1], 'y must be less than the second dimension');
-
-        if (!this.#checkMaxPop(x, y)) return null;
-        let newLocations = new Map([...this.locations].filter((row, r) =>
-            [...row].filter((column, c) =>
-                !(
-                    (r === x && c === y) ||
-                    (r === x - 1 && c === y) ||
-                    (r === x + 1 && c === y) ||
-                    (r === x && c === y - 1) ||
-                    (r === x && c === y + 1)
-                )
-            )
+    if (!this.#checkMaxPop(x, y)) return null;
+    let newLocations = new Map([...this.locations].filter((row, r) =>
+      [...row].filter((column, c) =>
+        !(
+          (r === x && c === y) ||
+          (r === x - 1 && c === y) ||
+          (r === x + 1 && c === y) ||
+          (r === x && c === y - 1) ||
+          (r === x && c === y + 1)
         )
-        );
+      )
+    )
+    );
 
-        return new Baloons(newLocations, this.dimensions);
-    }
+    return new Baloons(newLocations, this.dimensions);
+  }
 
-    /**
-     * returns baloon exists or not at the given location
-     * @param {int} x
-     * @param {int} y
-     * @returns {boolean}
-     */
-    isBaloonExists(x, y) {
-        return this.locations.get(x) && this.locations.get(x).get(y);
-    }
+  /**
+   * returns baloon exists or not at the given location
+   * @param {int} x
+   * @param {int} y
+   * @returns {boolean}
+   */
+  isBaloonExists(x, y) {
+    return this.locations.get(x) && this.locations.get(x).get(y);
+  }
 }
 
 export default Baloons;
--- src/utils/Balloons.test.js
+++ src/utils/Balloons.test.js
@@ -1,41 +1,41 @@
 import Baloons from './Balloons';
 
 test('constructor', () => {
-    const dimensions = [10, 10];
-    const locations = new Map([
-        [0, new Map([[0, 1], [1, 1], [4, 1], [9, 1]])],
-        [1, new Map([[0, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1]])],
-        [2, new Map([[0, 1], [1, 1], [4, 1], [9, 1]])],
-        [3, new Map([[0, 1], [1, 1]])],
-        [4, new Map([[0, 1], [1, 1]])],
-        [5, new Map([[0, 1], [1, 1]])],
-        [6, new Map([[0, 1], [1, 1]])],
-        [7, new Map([[0, 1], [1, 1]])],
-        [8, new Map([[0, 1], [1, 1]])],
-        [9, new Map([[0, 1], [1, 1]])],
-    ]);
-    const baloons = new Baloons(locations, dimensions);
-    expect(baloons).toBeInstanceOf(Baloons);
+  const dimensions = [10, 10];
+  const locations = new Map([
+    [0, new Map([[0, 1], [1, 1], [4, 1], [9, 1]])],
+    [1, new Map([[0, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1]])],
+    [2, new Map([[0, 1], [1, 1], [4, 1], [9, 1]])],
+    [3, new Map([[0, 1], [1, 1]])],
+    [4, new Map([[0, 1], [1, 1]])],
+    [5, new Map([[0, 1], [1, 1]])],
+    [6, new Map([[0, 1], [1, 1]])],
+    [7, new Map([[0, 1], [1, 1]])],
+    [8, new Map([[0, 1], [1, 1]])],
+    [9, new Map([[0, 1], [1, 1]])],
+  ]);
+  const baloons = new Baloons(locations, dimensions);
+  expect(baloons).toBeInstanceOf(Baloons);
 }
 );
 
 test('doPop', () => {
-    const dimensions = [10, 10];
-    const locations = new Map([
-        [0, new Map([[0, 1], [1, 1], [4, 1], [9, 1]])],
-        [1, new Map([[0, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1]])],
-        [2, new Map([[0, 1], [1, 1], [4, 1], [9, 1]])],
-        [3, new Map([[0, 1], [1, 1]])],
-        [4, new Map([[0, 1], [1, 1]])],
-        [5, new Map([[0, 1], [1, 1]])],
-        [6, new Map([[0, 1], [1, 1]])],
-        [7, new Map([[0, 1], [1, 1]])],
-        [8, new Map([[0, 1], [1, 1]])],
-        [9, new Map([[0, 1], [1, 1]])],
-    ]);
-    const baloons = new Baloons(locations, dimensions);
+  const dimensions = [10, 10];
+  const locations = new Map([
+    [0, new Map([[0, 1], [1, 1], [4, 1], [9, 1]])],
+    [1, new Map([[0, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1]])],
+    [2, new Map([[0, 1], [1, 1], [4, 1], [9, 1]])],
+    [3, new Map([[0, 1], [1, 1]])],
+    [4, new Map([[0, 1], [1, 1]])],
+    [5, new Map([[0, 1], [1, 1]])],
+    [6, new Map([[0, 1], [1, 1]])],
+    [7, new Map([[0, 1], [1, 1]])],
+    [8, new Map([[0, 1], [1, 1]])],
+    [9, new Map([[0, 1], [1, 1]])],
+  ]);
+  const baloons = new Baloons(locations, dimensions);
 
-    expect(baloons.doPop(0, 0)).toBe(null);
-    expect(baloons.doPop(1, 4)).toBeInstanceOf(Baloons);
+  expect(baloons.doPop(0, 0)).toBe(null);
+  expect(baloons.doPop(1, 4)).toBeInstanceOf(Baloons);
 }
 );
